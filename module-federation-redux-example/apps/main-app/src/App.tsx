f2c83638a9b76dd8d606d6e5c2abd99178328dc3
kimjong95 <mje807@naver.com>
2024-02-19 08:41:12 +0900

    main-app 작성

--- module-federation-redux-example/apps/main-app/src/App.tsx
+++ module-federation-redux-example/apps/main-app/src/App.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider, useDispatch, useSelector } from "react-redux";
 
-import './index.css'
+import "./index.css";
+import { decrement, increment } from "./redux/modules/counter";
+import createStore from "./redux/store";
 
-const App = () => (
-  <div className="container">
-    <div>Name: main-app</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+const { store, injectReducer } = createStore();
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
+const App = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector<ReturnType<typeof store.getState>, number>(
+    (state) => state.counter.value
+  );
 
-root.render(<App />)
+  return (
+    <div className="container">
+      <div>Name: main-app</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Empty CSS</div>
+      <div>{counter}</div>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+ReactDOM.createRoot(rootElement as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
